2a91bb226d01b11a3106ee483c22186cb4b99cf5
MuhammadJibranBabar <jibranbabar67@gmail.com>
2023-02-16 17:00:50 +0500

    add cors policy

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import cors from 'cors';
 import connectDB from './config/connectdb.js';
 import userRoutes from './routes/userRoutes.js';
 const app = express();
@@ -8,6 +9,12 @@ const port = process.env.PORT;
 const DATABASE_URL = process.env.DATABASE_URL;
 connectDB(DATABASE_URL);
 app.use(express.json());
+app.use(
+  cors({
+    origin: '*',
+    method: ['GET', 'POST'],
+  })
+);
 app.use('/api/user', userRoutes);
 
 app.listen(port, () => {
